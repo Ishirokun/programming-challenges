18f0682272a9fef8d4be4e6da1387a20a61fe534
Ishirokun <earljeroldcsanjose@gmail.com>
2024-08-02 09:18:12 +0800

    completed challenge #12

---
+++ WordCounter.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class WordCounter {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a sentence to count the words :");
+        String sentence = input.nextLine();
+        String[] words = sentence.split("\\s+");
+        List<String> trimmedWords = Arrays.stream(words).filter(x -> !x.isEmpty()).toList();
+        System.out.println("Word Count : " + trimmedWords.size());
+    }
+
+}
